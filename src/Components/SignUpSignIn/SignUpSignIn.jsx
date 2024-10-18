3ba94971d45424980b37fc9d591a9131999b1dc6
R-Yokesh <yokeshravichandran10@gmailcom>
2024-10-18 14:24:10 +0530

    Signup Page changes !!

--- src/Components/SignUpSignIn/SignUpSignIn.jsx
+++ src/Components/SignUpSignIn/SignUpSignIn.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
+import '../../Assets/Css/Sign.css'
 
 const SignUpSignIn = () => {
   return (
     <div>
-    <div className="language-selector">
-      <div className="divider"></div> 
-        <h2>Choose Your Language</h2> 
-        <h1> gotha gommala</h1>
-    </div>
-    </div>
-  )
-}
+      <div className="slanguage-selector">
+        <div className="divider"></div>
+        <h2>Sign in/Sign up</h2>
 
-export default SignUpSignIn
+        <input
+          type="text"
+          placeholder="Enter mobile number"
+          className="mobile-input"
+        />
+
+        <button className="submit-btn">Submit</button>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpSignIn;
--- src/Views/SignView.jsx
+++ src/Views/SignView.jsx
@@ -2,19 +2,21 @@ import React from 'react'
 import '../Assets/Css/Language.css'
 import DigiDocImage from '../Assets/Images/DigiDoc Head.png';
 import SplashImage from '../Assets/Images/Online Doctor-amico.png';
+import SignUpSignIn from '../Components/SignUpSignIn/SignUpSignIn';
 
 const SignView = () => {
   return (
     <div>
-        <div className="language-container">
-      <header className="logo-section">
-        <img src={DigiDocImage} alt="DigiDoc" className="logo" />
-      </header>
-      <div className="illustration-section">
-        <img src={SplashImage} alt="Doctor Illustration" className="illustration" />
-      </div>
+    <div className="language-container">
+    <header className="logo-section">
+      <img src={DigiDocImage} alt="DigiDoc" className="logo" />
+    </header>
+    <div className="illustration-section">
+      <img src={SplashImage} alt="Doctor Illustration" className="illustration" />
     </div>
-    </div>
+    <SignUpSignIn/>
+  </div>
+  </div>
   )
 }
 
