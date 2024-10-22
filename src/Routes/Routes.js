3719f0ae278f3cf791a8c7b6132be6b1e2ab9c66
R-Yokesh <yokeshravichandran10@gmailcom>
2024-10-22 17:43:38 +0530

    route setting !!

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -4,13 +4,17 @@ import Signinview from "../Views/SigninView";
 import FirstWelcomeView from "../Views/FirstWelcomeView";
 import OtpView from "../Views/OtpView"
 import UploadView from "../Views/UploadView";
+import MappingScreen from "../Views/MappingScreenView";
+import AutoMapSuccess from "../Views/AutoMapSuccessView"
 
 const RoutesData = [
   { path: "/", element: LangView, name: "Language Page" },    
   { path: "/signin", element: Signinview, name: "Sign In Page" },
   { path: "/otp", element: OtpView, name: "Sign In Page" },
   { path: "/welcome", element: FirstWelcomeView, name: "Welcome Page" },
-  { path: "/upload", element: UploadView, name: "Upload Page" }
+  { path: "/upload", element: UploadView, name: "Upload Page" },
+  { path: "/mapping", element: MappingScreen, name: "Mapping Page" },
+  { path: "/success", element: AutoMapSuccess, name: "Mapping Page" },
 
 ];
 
