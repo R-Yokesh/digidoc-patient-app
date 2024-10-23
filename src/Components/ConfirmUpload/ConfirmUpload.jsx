09d03d66b17c7cbdf4e2f741843d9d3097a5b40d
R-Yokesh <yokeshravichandran10@gmailcom>
2024-10-23 17:40:26 +0530

    ecd changes

---
+++ src/Components/ConfirmUpload/ConfirmUpload.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "../../Assets/Css/ConfirmUpload.css"
+import backIcon from "../../Assets/Images/Expand_left.png";
+import Med1 from "../../Assets/Images/Med1.png";
+import Med2 from "../../Assets/Images/Med2.png";
+import Med3 from "../../Assets/Images/Med3.png";
+
+
+const ConfirmUpload = () => {
+  return (
+    <div className="capp-container">
+      <div className="cupload-container">
+        <button className="cback-button">
+          <img src={backIcon} alt="Back" className="cicon-image" />
+        </button>
+
+        <h2 className="cdoctor-name">Dr. Luke Leon</h2>
+        <p className="cprescribed-medicines-heading">Prescribed Medicines</p>
+        <ul className="cmedicine-list">
+          <li>Paracetamol</li>
+          <li>Metformin</li>
+          <li>Amlodipine</li>
+        </ul>
+
+        <div className="cmedicine-list">
+          <div className="cmedicine-item">
+            <label>Medicine 1</label>
+            <div className="cmedicine-info">
+              <img
+                src={Med1}
+                alt="Medicine 1"
+                className="cmedicine-image"
+              />
+              <span>Table.jpg</span>
+              <button className="cdelete-button">×</button>
+            </div>
+          </div>
+
+          <div className="cmedicine-item">
+            <label>Medicine 2</label>
+            <div className="cmedicine-info">
+              <img
+                src={Med2}
+                alt="Medicine 2"
+                className="cmedicine-image"
+              />
+              <span>Table.jpg</span>
+              <button className="cdelete-button">×</button>
+            </div>
+          </div>
+
+          <div className="cmedicine-item">
+            <label>Medicine 3</label>
+            <div className="cmedicine-info">
+              <img
+                src={Med3}
+                alt="Medicine 3"
+                className="cmedicine-image"
+              />
+              <span>Table.jpg</span>
+              <button className="cdelete-button">×</button>
+            </div>
+          </div>
+        </div>
+
+        <button className="cnext-button">Next</button>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmUpload;
---
+++ src/Views/ConfirmUploadView.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ConfirmUpload from '../Components/ConfirmUpload/ConfirmUpload'
+
+const ConfirmUploadView = () => {
+  return (
+    <div>
+        <ConfirmUpload />
+    </div>
+  )
+}
+
+export default ConfirmUploadView
